af476d5fcf8ef292036119c927b755563c89b992
gtlewis <garethtlewis@gmail.com>
2017-01-03 21:07:02 +0100

    UI test

--- scripts/dreddit.js
+++ scripts/dreddit.js
@@ -1,3 +1,16 @@
 $(function(){
-	document.title = 'Dreddit - ' + web3.eth.defaultAccount;
+
+	var userAddress = web3.eth.defaultAccount;
+
+	if (userAddress != undefined) {
+		$("#user_div").text(userAddress);
+	}
+
+	var showAllSubreddits = false;
+
+	if (!showAllSubreddits && userAddress != undefined) {
+		document.title = 'Dreddit - ' + userAddress;
+	}
+
+	// TODO: show user's subscribed subreddits, or if ALL requested or user undefined then show all subreddits
 });
