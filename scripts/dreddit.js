a3784894385b8ef00f842f787ec238fc4f21d554
gtlewis <garethtlewis@gmail.com>
2017-01-04 18:11:02 +0100

    UI test

--- scripts/dreddit.js
+++ scripts/dreddit.js
@@ -2,14 +2,15 @@ $(function(){
 	// TODO: this all needs to be in web3 callback??
 	var contractAbi = [{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"subdreddits","outputs":[{"name":"name","type":"string"},{"name":"postCount","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"subdredditId","type":"uint256"}],"name":"unsubscribeUser","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"}],"name":"createSubdreddit","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"subdredditCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"subdredditId","type":"uint256"},{"name":"post","type":"string"}],"name":"addPostToSubdreddit","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"users","outputs":[{"name":"karma","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"subdredditId","type":"uint256"}],"name":"subscribeUser","outputs":[],"payable":false,"type":"function"}];
 	var contractAddress = '0xAE941Ac7D355ba6305F7E0939e70E1CD8FA4B2e1';
-	var contract = web3.eth.contract(contractAbi).at(contractAddress);
+	var contract;
 	var userAddress;
 	if (typeof web3 != 'undefined' && typeof web3.eth != 'undefined') {
+		contract = web3.eth.contract(contractAbi).at(contractAddress);
 		userAddress = web3.eth.accounts[0];
 	}
 	if (userAddress != undefined) {
 		$('#user').html('<a class="link" id="user" href="/posts.html?user=' + userAddress + '">' + userAddress + '</a>');
-		//TODO: set karma. contract.users(userAddress).karma;
+		$('#karma').text(contract.users(userAddress).karma);
 	}
 	if (current_page == 'front') {
 		if (userAddress != undefined) {
