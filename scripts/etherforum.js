2c174f36197bacd2eed3dedfbb88bfdee85207cc
gtlewis <garethtlewis@gmail.com>
2017-01-20 19:56:35 +0100

    UI update

--- scripts/etherforum.js
+++ scripts/etherforum.js
@@ -55,7 +55,7 @@ function showForumsPage() {
 										contract.getNameOfForum(forumId, function(error, forumName) {
 											if (!error) {
 												$('#no-forums-found').remove();
-												$('#content-main-titles').append('<h1 class="content-main-title"/>').append(displayForum(forumId, forumName));
+												$('#content-main-titles').append('<h1 class="content-main-title">' + displayForum(forumId, forumName).html() + '</h1>');
 											} else {
 												console.error(error);
 											}
