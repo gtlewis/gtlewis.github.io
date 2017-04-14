6a9dc0ec0dbcd5c2f926f94541b92fd27faeec35
gtlewis <garethtlewis@gmail.com>
2017-04-14 16:09:53 +0200

    Comments

--- scripts/etherforum.js
+++ scripts/etherforum.js
@@ -913,7 +913,7 @@ function displayComment(forumId, postId, commentId, isUpvoted, isDownvoted, upvo
 		commentBody = '[DELETED]';
 	}
 	var div1 = $('<div/>');
-	var div2 = $('<div class="comment-body"/>');
+	var div2 = $('<div id="comment-body-x" class="comment-body"/>');
 	var div3 = $('<div class="comment-vote"/>');
 	div3.append(displayCommentUpvote(forumId, postId, commentId, isUpvoted));
 	div3.append(displayCommentScore(upvoteCount, downvoteCount));
@@ -923,16 +923,15 @@ function displayComment(forumId, postId, commentId, isUpvoted, isDownvoted, upvo
 	var div4 = $('<div class="comment-info"/>');
 	div4.append(displayUser(commentOwner));
 	if (!isDeletedComment && commentOwner === currentUser) {
-		// TODO: edit???
-		div4.append($('<a href="#" onClick="createComment_(' + forumId + ', ' + postId + ');return false;">Edit</a>'));
+		div4.append($('<a href="#" onClick="$('#comment-body-x').prop('style', 'display:none');$('#edit-comment-body-x').prop('style', 'display:block');return false;">Edit</a>'));
 		div4.append($('<a href="#" onClick="contract.deleteComment(' + forumId + ', ' + postId + ', ' + commentId + ', void_callback);return false;">Delete</a>'));
-		var div5 = $('<div class="comment-body"/>');
-		div5.append($('<textarea class="edit-comment-text" rows="3" placeholder="Edit comment"></textarea>'));
+		var div5 = $('<div id="edit-comment-body-x" class="comment-body" style="display:none"/>');
+		div5.append($('<textarea id="edit-comment-text-x" class="edit-comment-text" rows="3" placeholder="Edit comment"></textarea>'));
 		var div6 = $('<div class="edit-comment-info"/>');
 		div6.append($('<a href="#" onclick="TODO;return false;">Submit</a>'));
-		div6.append($('<a href="#" onclick="TODO;return false;">Cancel</a>'));
+		div6.append($('<a href="#" onclick="$('#edit-comment-text-x').val('');$('#edit-comment-error-x').prop('style', 'visibility:hidden');$('#comment-body-x').prop('style', 'display:block');$('#edit-comment-body-x').prop('style', 'display:none');return false;">Cancel</a>'));
 		div6.append($('<a href="#" onclick="contract.deleteComment(' + forumId + ', ' + postId + ', ' + commentId + ', void_callback);return false;">Delete</a>'));
-		div7 = $('<div class="edit-comment-error">Error</div>');
+		div7 = $('<div class="edit-comment-error-x">Error</div>');
 		div6.append(div7);
 		div5.append(div6);
 		div1.append(div5);
