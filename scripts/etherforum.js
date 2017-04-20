b40fa92aacc7d8a209afe715efb19018f2727558
gtlewis <garethtlewis@gmail.com>
2017-04-21 00:39:35 +0200

    User balance

--- scripts/etherforum.js
+++ scripts/etherforum.js
@@ -28,7 +28,7 @@ window.addEventListener('load', function() {
 				$('#header-score-text').replaceWith(displayScore(score));
 				web3.eth.getBalance(currentUser, function(error, balance) {
 					if (!error) {
-						$('#header-user-balance').text(Math.round(web3.fromWei(balance)*100)/100);
+						$('#header-user-balance').text(Math.round(web3.fromWei(balance)*100)/100 + ' ETH');
 					} else {
 						console.error(error);
 					}
