e054ca1bd2baeeadef890ede8e4b7016b97c4faa
gtlewis <garethtlewis@gmail.com>
2017-05-14 02:31:11 +0200

    ENS

--- scripts/etherforum.js
+++ scripts/etherforum.js
@@ -1255,6 +1255,17 @@ function reverseENSLookup(user) {
 	return result;
 }
 
+function namehash(name) {
+    var node = '0x0000000000000000000000000000000000000000000000000000000000000000';
+    if (name !== '') {
+        var labels = name.split(".");
+        for(var i = labels.length - 1; i >= 0; i--) {
+            node = web3.sha3(node + web3.sha3(labels[i]).slice(2), {encoding: 'hex'});
+        }
+    }
+    return node.toString();
+}
+
 function void_callback(error, result) {
 	if (error) {
 		console.error(error);
